cdb7f340f77455f83bc7d34b1309c12105bf2d0d
DuvanGN <69404782+DuvanGN@users.noreply.github.com>
2022-03-29 13:00:44 -0500

    Delete prueba directory

--- prueba/src/main/java/com/prueba/repository/ILogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.prueba.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.prueba.model.Log;
-
-public interface ILogRepository extends CrudRepository<Log, Long> {
-}
--- prueba/src/main/java/com/prueba/repository/ITipoRespository.java
+++
@@ -1,7 +0,0 @@
-package com.prueba.repository;
-
-import com.prueba.model.Tipos;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ITipoRespository  extends CrudRepository<Tipos, Integer>  {
-}
